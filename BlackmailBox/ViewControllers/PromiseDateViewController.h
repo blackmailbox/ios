ab29fe54e6e1b32b1eb69c58f5ce616cfe7922e0
Steven Walker <me@iamwalker.com>
2013-03-04 13:45:50 -0800

    adding app icon

--- BlackmailBox/ViewControllers/PromiseDateViewController.h
+++ BlackmailBox/ViewControllers/PromiseDateViewController.h
@@ -15,6 +15,7 @@
 @property (nonatomic, weak) IBOutlet UITextView *numberOfDaysField;
 @property (nonatomic, retain) IBOutlet UILabel *promiseExpLabel;
 @property (nonatomic, retain) IBOutlet UITextView*promiseExpTextView;
+@property (nonatomic, retain) IBOutlet UILabel *promiseDaysLabel;
 
 @property (nonatomic, strong) Promise *promise;
 
